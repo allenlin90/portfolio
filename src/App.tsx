4ef845c3211ea8d90bd8c44a49bc690a2ba2cd68
allenlin <ht016746@gmail.com>
2024-08-25 10:32:31 +0700

    chore: error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import { Routes, Route } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary';
 
 import { Routes as Paths } from '@/constants/routes';
 
 import { HomePage } from '@/pages/home';
-import { ImageSliderPage } from '@/pages/image-slider';
+import { ImageSliderPage } from '@/pages/image-slider/image-slider';
+
+import { Fallback } from '@/components/Fallback';
 
 function App() {
   return (
-    <Routes>
-      <Route index element={<HomePage />} />
-      <Route path={Paths.UI}>
-        <Route path={Paths.ImageSlider} element={<ImageSliderPage />} />
-      </Route>
-    </Routes>
+    <ErrorBoundary FallbackComponent={Fallback}>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path={Paths.UI}>
+          <Route path={Paths.ImageSlider} element={<ImageSliderPage />} />
+        </Route>
+      </Routes>
+    </ErrorBoundary>
   );
 }
 
---
+++ src/components/Fallback.tsx
@@ -0,0 +1,40 @@
+import { useCallback } from 'react';
+import { Link } from 'react-router-dom';
+import { FallbackProps as ErrorFallbackProps } from 'react-error-boundary';
+import { type VariantProps } from 'class-variance-authority';
+
+import { cn } from '@/lib/utils';
+import { Routes } from '@/constants/routes';
+import { buttonVariants } from '@/components/ui/button';
+
+interface FallbackProps
+  extends ErrorFallbackProps,
+    VariantProps<typeof buttonVariants>,
+    React.HTMLAttributes<HTMLAnchorElement> {}
+
+export const Fallback: React.FC<FallbackProps> = ({
+  error,
+  resetErrorBoundary,
+  variant,
+  size,
+  className,
+  ...props
+}) => {
+  const onClick = useCallback(() => resetErrorBoundary(), [resetErrorBoundary]);
+
+  return (
+    <div className='w-full h-dvh flex flex-col justify-center items-center'>
+      <div>{error.message}</div>
+      <Link
+        to={Routes.Home}
+        onClick={onClick}
+        className={cn(buttonVariants({ variant, size, className }))}
+        {...props}
+      >
+        Back to Home
+      </Link>
+    </div>
+  );
+};
+
+Fallback.displayName = 'Fallback';
